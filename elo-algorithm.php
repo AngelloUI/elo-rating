a38293f85f585f82240b87196a1cb5568f78cf4e
Dmitry <usefullforit@gmail.com>
2023-03-11 15:48:32 +0300

    elo-algorithm

--- elo-algorithm.php
+++ elo-algorithm.php
@@ -1,2 +1,8 @@
 <?php
-
+function countRating(&$Ra, &$Rb, $Sa, $k = 16): void
+{
+    $Ea = 1 / (1 + pow(10, (($Rb - $Ra) / 400)));
+    $tempRa = $Ra;
+    $Ra = round($Ra + $k * ($Sa - $Ea));
+    $Rb = round($Rb + $tempRa - $Ra);
+}
